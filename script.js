02739e26e9ddbcea8c1586ed81d39b739cc517f3
me-junaid <140057488+me-junaid@users.noreply.github.com>
2024-04-14 23:22:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myClose(){
+  document.getElementById("blackish").style.display = 'none';
+  document.getElementById("popup").style.display = 'none';
+}
+
+function mySorry(){
+  document.getElementById("overThe").style.display = 'block';
+}
